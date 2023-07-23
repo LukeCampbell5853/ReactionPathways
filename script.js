ce6370cbfbeb2014c1d442fe30e7062373ff3380
Luke Campbell <64815494+LukeCampbell5853@users.noreply.github.com>
2023-07-23 14:29:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const diagram = document.getElementById("diagram");
 diagram.addEventListener("load", function() {
   const svg = diagram.contentDocument;
 
-  const ids = ["alkanes", "alkenes", "alkynes", "haloalkanes", "sugars", "alcohols", "aldehydes", "ketones", "carboxylic_acids", "amides", "amines", "esters", "carboxylate_ions", "alkanes_haloalkanes", "alkenes_alkanes", "alkenes_haloalkanes", "alkenes_alcohols", "alkynes_haloalkanes", "alkynes_alkenes", "haloalkanes_alcohols", "alcohols_haloalkanes", "alcohols_alkenes", "alcohols_ketones", "alcohols_esters", "alcohols_aldehydes", "sugars_alcohols", "aldehydes_carboxylic_acids", "carboxylic_acids_carboxylate_ions", "carboxylic_acids_amides", "alcohols_carboxylic_acids"]
+  const ids = ["alkanes", "alkenes", "alkynes", "haloalkanes", "sugars", "alcohols", "aldehydes", "ketones", "carboxylic_acids", "amides", "amines", "esters", "carboxylate_ions", "alkanes_haloalkanes", "alkenes_alkanes", "alkenes_haloalkanes", "alkenes_alcohols", "alkynes_haloalkanes", "alkynes_alkenes", "haloalkanes_alcohols", "alcohols_haloalkanes", "alcohols_amines", "alcohols_alkenes", "alcohols_ketones", "alcohols_esters", "alcohols_aldehydes", "sugars_alcohols", "aldehydes_carboxylic_acids", "carboxylic_acids_carboxylate_ions", "carboxylic_acids_amides", "alcohols_carboxylic_acids"]
 
   for (i = 0; i < ids.length; i++) {
     make_popup(svg, ids[i])
